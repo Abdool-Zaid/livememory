80e1d8c2cef76f695f17673773fc0317d887be9a
Abdool-Zaid <aztoorabally7447@gmail.com>
2022-10-27 16:28:43 +0200

    'home'

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,44 @@
 <script setup>
-
-</script>
-
+import NavBar from "./components/NavBar.vue";
+import { onMounted } from "vue";
+components: { NavBar }
+let imageSRC='https://picsum.photos/100/100?random='
+let i
+let compareValues=[]
+let checkResults=(src)=>{
+  console.log('checking')
+// if(compareValues.length<2){
+//   compareValues.push(src)
+// }else if(compareValues.length=2){
+//   if(compareValues[0]==compareValues[1]){
+//     console.log('correct')
+//   compareValues=[]
+//   }
+// else{
+//   console.log('incorrect')
+//   compareValues=[]
+// }
+// }
+}
+onMounted(() => {
+  //   <img src="https://picsum.photos/200/300?random=1">
+// <img src="https://picsum.photos/200/300?random=2">
+// field.innerHTML='
+for ( i = 0; i < 10; i++) {
+field.innerHTML+=`
+<img src="${imageSRC+i}" onclick="checkResults(${i})">
+<img src="${imageSRC+i}" onclick="checkResults(${i})">
+`  
+}
+})
+  </script>
 <template>
 <button class="btn " type="button" data-bs-toggle="offcanvas" id="navOffCanvas" data-bs-target="#offcanvasExample" aria-controls="offcanvasExample">
 Memory tester</button>
+<nav-bar></nav-bar>
+<div id="field" class=""></div>
 
-<div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
-  <div class="offcanvas-header">
-    <h5 class="offcanvas-title" id="offcanvasExampleLabel">Offcanvas</h5>
-    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-  </div>
-  <div class="offcanvas-body">
-    <div>
-      Some text as placeholder. In real life you can have the elements you have chosen. Like, text, images, lists, etc.
-    </div>
-    <div class="dropdown mt-3">
-      <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown">
-        Dropdown button
-      </button>
-      <ul class="dropdown-menu">
-        <li><a class="dropdown-item" href="#">Action</a></li>
-        <li><a class="dropdown-item" href="#">Another action</a></li>
-        <li><a class="dropdown-item" href="#">Something else here</a></li>
-      </ul>
-    </div>
-  </div>
-</div></template>
+</template>
 
 <style>
 *{
@@ -43,5 +55,14 @@ body{
   top:0;
   left:0;
   margin: 1%;
+}
+#field{
+  position: fixed;
+  left: 50%;
+  top: 50%;
+ transform: translate(-50%,-50%);
+  width:70vw;
+  height: 80vh;
+  background-color: green;
 }
 </style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,21 @@
+<script setup>
+</script>
+<template>
+  
+  <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
+    <div class="offcanvas-header">
+      <h5 class="offcanvas-title" id="offcanvasExampleLabel"></h5>
+      <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+    </div>
+    <div class="offcanvas-body">
+      <div>
+        <img src="https://raw.githubusercontent.com/Abdool-Zaid/livememory/main/src/assets/download.png" class="img-fluid" alt="live memory logo">    </div>
+      <div class="dropdown mt-3">
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+
+</style>
